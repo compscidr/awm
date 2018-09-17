a4773b0151da48f089bd11cd9ea099eb96932ca7
Jason Ernst <jason@left.io>
2018-09-17 15:21:13 -0700

    initial commit of code

--- awm/src/main/java/io/rightmesh/awm/AndroidWirelessStatsCollector.java
+++ awm/src/main/java/io/rightmesh/awm/AndroidWirelessStatsCollector.java
@@ -1,4 +1,12 @@
 package io.rightmesh.awm;
 
+import android.util.Log;
+
 public class AndroidWirelessStatsCollector {
+
+    public static final String TAG = AndroidWirelessStatsCollector.class.getCanonicalName();
+
+    public AndroidWirelessStatsCollector() {
+        Log.i(TAG, "Initializing Android Wireless Stats Collector");
+    }
 }
