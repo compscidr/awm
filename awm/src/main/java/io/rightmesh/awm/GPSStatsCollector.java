3aaf7c1d9fa05ff168129af2da7320fd2f008beb
Jason Ernst <jason@left.io>
2018-09-25 06:54:43 -0700

    Added code that makes the GPS updates actually function

--- awm/src/main/java/io/rightmesh/awm/GPSStatsCollector.java
+++ awm/src/main/java/io/rightmesh/awm/GPSStatsCollector.java
@@ -1,27 +1,75 @@
 package io.rightmesh.awm;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
+import android.os.Looper;
+import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.location.LocationSettingsRequest;
 
-public class GPSStatsCollector extends StatsCollector {
+/**
+ * https://stackoverflow.com/questions/28535703/best-way-to-get-user-gps-location-in-background-in-android
+ */
+class GPSStatsCollector extends StatsCollector {
+
+    private Context context;
+    private static final String TAG = AndroidWirelessStatsCollector.class.getCanonicalName();
+
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 1000;
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = 1000;
 
     private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest locationRequest;
+    private LocationSettingsRequest locationSettingsRequest;
+    private LocationCallback locationCallback;
 
     GPSStatsCollector(Context context) {
+        this.context = context;
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+        locationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(this.locationRequest);
+        locationSettingsRequest = builder.build();
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult); // why? this. is. retarded. Android.
+                Location currentLocation = locationResult.getLastLocation();
+
+                Log.i(TAG, "Location Callback results: " + currentLocation.getLatitude()
+                        + " " + currentLocation.getLongitude());
+            }
+        };
     }
 
     @Override
     void start() throws Exception {
-        mFusedLocationClient.requestLocationUpdates()
+        if(Build.VERSION.SDK_INT >= 23) {
+            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && context.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                throw new Exception("No permission for location, cannot get GPS positions");
+            }
+        }
+
+        mFusedLocationClient.requestLocationUpdates(this.locationRequest,
+                this.locationCallback, Looper.myLooper());
     }
 
     @Override
     void stop() {
-
+        mFusedLocationClient.removeLocationUpdates(locationCallback);
     }
 }
