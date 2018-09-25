9421b058cf22a96af84c0cf9a8a1060fe439d19e
Jason Ernst <jason@left.io>
2018-09-24 19:27:42 -0700

    Added an abstract stat collector class that has common parts that all stats collectors will use

---
+++ awm/src/main/java/io/rightmesh/awm/StatsCollector.java
@@ -0,0 +1,20 @@
+package io.rightmesh.awm;
+
+import com.anadeainc.rxbus.Bus;
+import com.anadeainc.rxbus.BusProvider;
+import com.vanniktech.rxpermission.Permission;
+
+import java.util.Map;
+
+public abstract class StatsCollector {
+
+    protected Bus eventBus = BusProvider.getInstance();
+    Map<String, Permission> permissions = null;
+
+    void setPermissions(Map<String, Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    abstract void start() throws Exception;
+    abstract void stop();
+}
