e81365801c4c5d5cd3bfd73cd0fa1ef0d51e3236
Jason Ernst <jason@left.io>
2018-09-25 07:36:27 -0700

    Added the GPSStats class for gps data

---
+++ awm/src/main/java/io/rightmesh/awm/GPSStats.java
@@ -0,0 +1,11 @@
+package io.rightmesh.awm;
+
+public class GPSStats {
+    public double longitude;
+    public double latitude;
+
+    GPSStats(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+}
