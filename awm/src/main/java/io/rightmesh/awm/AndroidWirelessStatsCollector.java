e83f4b4a93ce051e1e089a2871b1f85215932a50
Jason Ernst <jason@left.io>
2019-01-28 19:25:04 -0800

    listen for the updates in the main class and call the network or disk logging to store the results

--- awm/src/main/java/io/rightmesh/awm/AndroidWirelessStatsCollector.java
+++ awm/src/main/java/io/rightmesh/awm/AndroidWirelessStatsCollector.java
@@ -1,9 +1,15 @@
-package io.rightmesh.awm.collectors;
+package io.rightmesh.awm;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.util.Log;
 
+import com.anadeainc.rxbus.Bus;
+import com.anadeainc.rxbus.BusProvider;
+import com.anadeainc.rxbus.Subscribe;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.vanniktech.rxpermission.Permission;
@@ -13,26 +19,46 @@ import com.vanniktech.rxpermission.RxPermission;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.CompositeDisposable;
+import io.rightmesh.awm.collectors.BluetoothStatsCollector;
+import io.rightmesh.awm.collectors.GPSStatsCollector;
+import io.rightmesh.awm.collectors.InternetStatsCollector;
+import io.rightmesh.awm.collectors.StatsCollector;
+import io.rightmesh.awm.collectors.WiFiAPStatsCollector;
+import io.rightmesh.awm.collectors.WiFiDirectStatsCollector;
+import io.rightmesh.awm.loggers.DiskLogger;
+import io.rightmesh.awm.loggers.NetworkLogger;
 import io.rightmesh.awm.loggers.StatsLogger;
+import io.rightmesh.awm.stats.BluetoothStats;
+import io.rightmesh.awm.stats.GPSStats;
+import io.rightmesh.awm.stats.NetworkStat;
+import io.rightmesh.awm.stats.WiFiStats;
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 
 public class AndroidWirelessStatsCollector {
 
+    private boolean uploadImmediately = false;
+    private static final String SHARED_PREF_FILE = "uuid.dat";
     private static final String TAG = AndroidWirelessStatsCollector.class.getCanonicalName();
     private Set<StatsCollector> statsCollectors;
-    private StatsLogger statsLogger;
+    private Set<StatsLogger> statsLoggers;
+    private Bus eventBus = BusProvider.getInstance();
 
     //permission checking code
     private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 101;
     private RxPermission rxPermission;
     private @NonNull final CompositeDisposable compositeDisposable = new CompositeDisposable();
     private HashMap<String, Permission> permissionResults;
+
     //all of the permissions that the library needs
-    String[] permissions = { Manifest.permission.BLUETOOTH,
+    private String[] permissions = { Manifest.permission.BLUETOOTH,
             Manifest.permission.BLUETOOTH_ADMIN,
             ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION,
@@ -46,44 +72,71 @@ public class AndroidWirelessStatsCollector {
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
 
-    public AndroidWirelessStatsCollector(Activity activity) {
+    private NetworkLogger networkLogger;
+    private DiskLogger diskLogger;
+
+    private ScheduledExecutorService scheduleTaskExecutor;
+    private ObservingDevice thisDevice;
+    private SharedPreferences sharedPreferences;
+
+    public AndroidWirelessStatsCollector(Activity activity, boolean uploadImmediately) {
         Log.i(TAG, "Initializing Android Wireless Stats Collector");
 
+        this.uploadImmediately = uploadImmediately;
+
+        sharedPreferences = activity.getApplicationContext()
+                .getSharedPreferences(SHARED_PREF_FILE, Context.MODE_PRIVATE);
+
+        scheduleTaskExecutor = Executors.newScheduledThreadPool(5);
+
+        UUID uuid;
+        String uuidString = sharedPreferences.getString("uuid", null);
+        if(uuidString == null) {
+            uuid = UUID.randomUUID();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("uuid", uuid.toString());
+            editor.apply();
+        } else {
+            uuid = UUID.fromString(uuidString);
+        }
+
+        //detect Android OS version
+        String OS = Build.MANUFACTURER + " " + Build.MODEL + " " + Build.VERSION.SDK_INT + " "
+                + Build.VERSION.RELEASE;
+        thisDevice = new ObservingDevice(uuid, OS);
+
         statsCollectors = new HashSet<>();
+        statsLoggers = new HashSet<>();
+
         rxPermission = RealRxPermission.getInstance(activity.getApplicationContext());
 
-        //get position so we can log where we are encountering what devices
         GPSStatsCollector gpsStats = new GPSStatsCollector(activity.getApplicationContext());
         statsCollectors.add(gpsStats);
 
-        //bluetooth stats
         BluetoothStatsCollector btStats = new BluetoothStatsCollector(activity.getApplicationContext());
         statsCollectors.add(btStats);
 
-        //WiFi AP stats
         WiFiAPStatsCollector wifiStats = new WiFiAPStatsCollector(activity.getApplicationContext());
         statsCollectors.add(wifiStats);
 
-        //WiFi Direct stats
         WiFiDirectStatsCollector wifiDirectStats = new WiFiDirectStatsCollector(activity.getApplicationContext());
         statsCollectors.add(wifiDirectStats);
 
-        //internet stats
         InternetStatsCollector itStats = new InternetStatsCollector(activity.getApplicationContext());
         statsCollectors.add(itStats);
 
-        //logger to disk and server
-        statsLogger = new StatsLogger(activity.getApplicationContext());
+        networkLogger = new NetworkLogger();
+        statsLoggers.add(networkLogger);
 
-        //position stats
+        diskLogger = new DiskLogger(activity.getApplicationContext());
+        statsLoggers.add(diskLogger);
+
         if (!checkPlayServices(activity)) {
             Log.d(TAG, "Missing Google Play Services - GPS likely won't work.");
         }
     }
 
     public void start() {
-        statsLogger.start();
-
         Log.i(TAG, "Checking permissions");
         permissionResults = new HashMap<>();
         compositeDisposable.add(rxPermission.requestEach(permissions)
@@ -93,9 +146,13 @@ public class AndroidWirelessStatsCollector {
 
                     //see if we've received all the permissions yet
                     if(permissionResults.size() == permissions.length) {
+                        startLoggers();
                         startStats();
                     }
                 }));
+        eventBus.register(this);
+        //scheduleTaskExecutor.scheduleAtFixedRate(
+        //        this::uploadDisk, 0, 10, TimeUnit.SECONDS);
     }
 
     /**
@@ -118,15 +175,27 @@ public class AndroidWirelessStatsCollector {
         return true;
     }
 
+    private void startLoggers() {
+        Log.i(TAG, "Starting stats loggers");
+        for(StatsLogger statsLogger : statsLoggers) {
+            try {
+                statsLogger.start();
+            } catch(Exception ex) {
+                Log.e(TAG, "Exception starting the stats logger: " + ex.toString());
+                ex.printStackTrace();
+            }
+        }
+    }
+
     private void startStats() {
         Log.i(TAG, "Starting stats collection");
         for(StatsCollector statsInterface : statsCollectors) {
             statsInterface.setPermissions(permissionResults);
-
             try {
                 statsInterface.start();
             } catch(Exception ex) {
                 Log.d(TAG, "Exception starting the stats collection: " + ex.toString());
+                ex.printStackTrace();
             }
         }
     }
@@ -139,6 +208,57 @@ public class AndroidWirelessStatsCollector {
             statsInterface.stop();
         }
 
-        statsLogger.stop();
+        for(StatsLogger statsLogger: statsLoggers) {
+            statsLogger.stop();
+        }
+    }
+
+    @Subscribe
+    public void updateNetworkStats(NetworkStat networkStat) {
+        Log.d(TAG, "UPDATE NETWORK STATS");
+        if(networkStat instanceof WiFiStats) {
+            thisDevice.setWifiMac(networkStat.getMac());
+        } else if(networkStat instanceof BluetoothStats) {
+            thisDevice.setBluetoothMac(networkStat.getMac());
+        }
+
+        try {
+            if (uploadImmediately) {
+                networkLogger.log(networkStat, thisDevice);
+            } else {
+                diskLogger.log(networkStat, thisDevice);
+            }
+        } catch(Exception ex) {
+            try {
+                diskLogger.log(networkStat, thisDevice);
+            } catch(Exception ex2) {
+                Log.e(TAG, "Failed to log to network and disk. Results lost: "
+                        + ex2.toString());
+                ex2.printStackTrace();
+            }
+        }
+    }
+
+    @Subscribe
+    public void updateGPS(GPSStats gpsStats) {
+        thisDevice.updatePosition(gpsStats);
+    }
+
+    private void uploadDisk() {
+        String jsondata;
+        try {
+            jsondata = diskLogger.getPendingLogs();
+        } catch(Exception ex) {
+            Log.d(TAG, "Error reading disk logs: " + ex.toString());
+            ex.printStackTrace();
+            return;
+        }
+
+        try {
+            networkLogger.uploadPendingLogs(jsondata);
+        } catch(Exception ex) {
+            Log.d(TAG, "Unable to upload presently: " + ex.toString());
+            ex.printStackTrace();
+        }
     }
 }
