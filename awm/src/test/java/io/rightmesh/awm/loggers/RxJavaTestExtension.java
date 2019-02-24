f967055325b1695c099cd057611fa1f1bf72bcc8
Jason Ernst <jason@left.io>
2019-02-23 17:32:19 -0800

    added this so that we can test with the eventbus

--- awm/src/test/java/io/rightmesh/awm/loggers/RxJavaTestExtension.java
+++ awm/src/test/java/io/rightmesh/awm/loggers/RxJavaTestExtension.java
@@ -1,4 +1,47 @@
 package io.rightmesh.awm.loggers;
 
-public class RxJavaTestExtension {
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.plugins.RxAndroidPlugins;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.internal.schedulers.ExecutorScheduler;
+import io.reactivex.plugins.RxJavaPlugins;
+
+/**
+ * Required to handle the getLooper() stuff in RxJava.
+ * https://stackoverflow.com/questions/43356314/android-rxjava-2-junit-test-getmainlooper-in-android-os-looper-not-mocked-runt
+ * https://www.infoq.com/articles/Testing-RxJava2
+ * https://medium.com/@peter.tackage/overriding-rxandroid-schedulers-in-rxjava-2-5561b3d14212
+ * https://stackoverflow.com/questions/43282798/in-junit-5-how-to-run-code-before-all-tests
+ *
+ * use with the ExtendWith annotation.
+ */
+public class RxJavaTestExtension implements BeforeAllCallback {
+
+    @Override
+    public void beforeAll(ExtensionContext context) throws Exception {
+        Scheduler immediate = new Scheduler() {
+            @Override
+            public Disposable scheduleDirect(@NonNull Runnable run, long delay, @NonNull TimeUnit unit) {
+                // this prevents StackOverflowErrors when scheduling with a delay
+                return super.scheduleDirect(run, 0, unit);
+            }
+
+            @Override
+            public Scheduler.Worker createWorker() {
+                return new ExecutorScheduler.ExecutorWorker(Runnable::run);
+            }
+        };
+
+        RxJavaPlugins.setInitIoSchedulerHandler(scheduler -> immediate);
+        RxJavaPlugins.setInitComputationSchedulerHandler(scheduler -> immediate);
+        RxJavaPlugins.setInitNewThreadSchedulerHandler(scheduler -> immediate);
+        RxJavaPlugins.setInitSingleSchedulerHandler(scheduler -> immediate);
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler(scheduler -> immediate);
+    }
 }
