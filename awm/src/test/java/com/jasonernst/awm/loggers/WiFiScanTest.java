eda7c520621e9d4224874f2328c9f265864d3da2
Jason Ernst <ernstjason1@gmail.com>
2021-04-27 08:59:54 -0700

    Fixes for jacoco reports

---
+++ awm/src/test/java/com/jasonernst/awm/loggers/WiFiScanTest.java
@@ -0,0 +1,10 @@
+package com.jasonernst.awm.loggers;
+
+import org.junit.jupiter.api.Test;
+
+public class WiFiScanTest {
+    @Test
+    public void WiFiScan() {
+        WiFiScan wiFiScan = new WiFiScan();
+    }
+}
