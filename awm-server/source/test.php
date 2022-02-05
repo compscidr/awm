4e16acf8cf1dc4c1596f94013546d5f04619c73a
Jason Ernst <ernstjason1@gmail.com>
2022-02-05 12:04:49 -0800

    re-org backend files according to endpoint spec

---
+++ awm-server/source/test.php
@@ -0,0 +1,27 @@
+<?php
+require_once("common.php");
+
+$mysqli = attemptConnect();
+if($mysqli->connect_error){
+  http_response_code(503);
+  echo "Problem connecting to the database to store the data";
+  exit;
+}
+
+$sql = "SELECT * FROM reporting_device";
+$result = $mysqli->query($sql);
+if($result !== false && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $reporting_id = $row["id"];
+        $sql2 = "SELECT * FROM observed_device WHERE reporting_device_id = $reporting_id";
+        $result2 = $mysqli->query($sql2);
+        $long = $row["longitude"];
+        $lat = $row["latitude"];
+        echo "FOUND ".$result2->num_rows." devices at ".$lat.",".$long."<br/>\n";
+    }
+}
+
+// want to find the average number of devices at particular latlng from all measurements
+
+
+?>
