4c273fa89b82a6fdff2ad6ef6bd6be528a1f7c22
Jason Ernst <ernstjason1@gmail.com>
2022-04-06 18:36:14 -0700

    added some models and controllers

--- awm-server/routes/web.php
+++ awm-server/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('reporting_devices', ReportingDeviceController::class);
+Route::resource('observed_devices', ObservedDeviceController::class);
