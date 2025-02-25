eaf45407260bb5a1205e9f1f5138fd2d5c5533dd
Jason Ernst <ernstjason1@gmail.com>
2025-02-25 09:32:42 -0800

    fixed with bom

--- awm-common/build.gradle.kts
+++ awm-common/build.gradle.kts
@@ -24,6 +24,7 @@ java {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.12.0")
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.bundles.test)
+    testRuntimeOnly(libs.bundles.test.runtime)
 }
