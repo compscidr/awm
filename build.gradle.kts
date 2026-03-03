e6525a78d897f577a7d34c507639819ca653bed9
Jason Ernst <ernstjason1@gmail.com>
2026-03-03 08:28:02 -0800

    Remove org.jetbrains.kotlin.android plugin for AGP 9 compatibility
    AGP 9.0 has built-in Kotlin support, making the org.jetbrains.kotlin.android
    plugin unnecessary and incompatible. Removing it as per the migration guide:
    https://developer.android.com/build/migrate-to-built-in-kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     //alias(libs.plugins.quarkus) apply false // this causes "Could not initialize class com.android.sdklib.repository.AndroidSdkHandler"
     alias(libs.plugins.kotlinx.serialization) apply false
